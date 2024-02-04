205a6edf1c9c3ff980926559568ec1c17bfd4cf8
unknown <2369421235@qq.com>
2024-02-05 00:11:25 +0800

    project build

---
+++ testProceduce/Model/People.cs
@@ -0,0 +1,10 @@
+﻿namespace testProceduce.Model
+{
+    public class people
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; }
+
+    }
+}
